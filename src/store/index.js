6aa9423c4d0d8a30a64b7bd9a3ee5ef07104e44e
ken <ken@mindnaked.com>
2018-10-06 12:50:35 +0800

    Remove the vuex-persist. Dont need it for now.

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import VuexPersistence from 'vuex-persist';
 import rateModule from '@/store/modules/rateModule';
 
 Vue.use(Vuex);
 
-const vuexLocal = new VuexPersistence({
-    key: 'vuex',
-    storage: window.localStorage
-});
-
 const store = new Vuex.Store({
     modules: {
         rateModule
-    },
-    plugins: [vuexLocal.plugin]
+    }
 });
 
 export default store;
